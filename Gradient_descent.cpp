8daadb4d00e7613411d1407d633de6bb841c2c7a
AdoNoColor <macccich@gmail.com>
2022-04-28 14:02:56 +0300

    fix: general method's x_prev dilemma add: primitive function class

--- Gradient_descent.cpp
+++ Gradient_descent.cpp
@@ -1,25 +1,29 @@
 #include "h/Gradient_descent.h"
 #include "h/Functions.h"
 #include "h/Steps.h"
+#include "h/ParabolaWithTwoArguments.h"
 
-std::vector<double> GradientDescent(double x, double y, double accuracy, double step) {
+std::vector<double> GradientDescent(class ParabolaWithTwoArguments f, double accuracy, double step) {
     int iteration = 0;
-    std::vector<double> x_prev = {
-            x, y,
-    };
 
-    std::vector<double> x_next = {
-            x+accuracy, y+accuracy,
-    };
+    std::vector<double> x_prev = f.getArgs();
+
+    std::vector<double> x_next = x_prev;
 
     double a = ConstantStep(step);
 
-    while (std::abs(ParabolaWithTwoArguments(x_next) - ParabolaWithTwoArguments(x_prev)) >= accuracy) {
+    std::vector<double> grad = f.Gradient(x_prev);
+
+    for (int i = 0; i < 2; i++) {
+        x_next[i] = x_prev[i]-a*grad[i];
+    }
+
+    while (std::abs(f.Result(x_next) - f.Result(x_prev)) >= accuracy) {
         iteration++;
 
         x_prev = x_next;
         a = ConstantStep(step);
-        std::vector<double> grad = ParabolaWithTwoArgumentsGradient(x_prev);
+        std::vector<double> grad = f.Gradient(x_prev);
 
         for (int i = 0; i < 2; i++) {
                x_next[i] = x_prev[i]-a*grad[i];
---
+++ ParabolaWithTwoArguments.cpp
@@ -0,0 +1,36 @@
+//
+// Created by Иванов on 28.04.2022.
+//
+
+#include <cmath>
+#include <vector>
+#include "h/ParabolaWithTwoArguments.h"
+
+
+ParabolaWithTwoArguments::ParabolaWithTwoArguments(double x, double y){
+    ParabolaWithTwoArguments::args = {
+                x,
+                y,
+        };
+    }
+
+const std::vector<double> &ParabolaWithTwoArguments::getArgs() const {
+    return args;
+}
+
+void ParabolaWithTwoArguments::setArgs(const std::vector<double> &args) {
+    ParabolaWithTwoArguments::args = args;
+}
+
+double ParabolaWithTwoArguments::Result(const std::vector<double> &args) {
+        return std::pow(args[0], 2) + 3*std::pow(args[1],2) - 2 * args[0]*args[1]+1;
+    }
+
+std::vector<double> ParabolaWithTwoArguments::Gradient(const std::vector<double> &args) {
+        std::vector<double> result {
+            2*args[0]-2*args[1],
+            6*args[1]-2*args[0]
+        };
+
+        return result;
+    }
